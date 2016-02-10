4091ab4bfcb643a30b0805bc6e06e1a61e2ec59b
Jason Booras <jasonbooras@gmail.com>
2016-02-10 15:57:43 -0500

    Remove duplicate attendances

--- db/migrate/20160210173253_create_attendances.rb
+++
@@ -1,9 +0,0 @@
-class CreateAttendances < ActiveRecord::Migration
-  def change
-    create_table :attendances do |t|
-      t.integer :status, default: 0, index: true
-
-      t.timestamps null: false
-    end
-  end
-end

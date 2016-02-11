45221e7846b456e162a05e4959e3a362a2298f25
Dana Gill <d.gill.design@gmail.com>
2016-02-11 07:40:19 -0500

    Makes basic course and student views work

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :courses
   resources :attendances
   resources :users
+  resources :students
+  resources :producers
+  resources :instructors
 
   get '/login', to: 'users#index'
 end

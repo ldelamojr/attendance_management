c7bac6e1b817bf437e4e19f1a82c453a905d5318
Leonel Delamo <ldelamojr@gmail.com>
2016-02-14 19:37:49 -0500

    Add code to attempt to accomodate automated reply form instructor to student.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -132,12 +132,27 @@ class CoursesController < ApplicationController
       @students = Student.where(:id => course_user_ids )
 
     end
+
+    def receive_sms
+      content_type 'text/xml'
+
+      response = Twilio::TwiML::Response.new do |r|
+        r.Message "Thank you, your message has been posted to today's roll sheet."
+      end
+
+      response.to_xml
+    end
+
   end
+
 
   def contact
 binding.pry
-    account_sid = ""
-    auth_token = ""
+    account_sid = "AC00822a76118217c7fe71b91ebb891298"
+    auth_token = "c95ad825e174c475ab8fc966ebdda9dc"
+
+    # account_sid = ""
+    # auth_token = ""
     client = Twilio::REST::Client.new account_sid, auth_token
      
     from = "+14155992671"  # "+17868027784"   #  # Your Twilio number +13473531559
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   get '/', to: 'users#index'
   post '/login', to: 'users#login'
   post '/contact', to: 'courses#contact'
+  get '/receive_sms', to: 'courses#receive_sms'
 
 end

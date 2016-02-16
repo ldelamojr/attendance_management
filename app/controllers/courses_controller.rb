333cd4a6395387fc5e3c7f9be6f2682e8aed8683
Leonel Delamo <ldelamojr@gmail.com>
2016-02-16 10:39:40 -0500

    Remove Sinatra since it was unnecessary.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,5 @@
 require 'twilio-ruby'
-require 'sinatra'
+# require 'sinatra'
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
 
